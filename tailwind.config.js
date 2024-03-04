ae46dfaabd6de693c2eb0a21df32adfdab86b129
Aaron Craelius <aaronc@users.noreply.github.com>
2024-03-04 16:34:13 -0500

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,26 +49,7 @@ export default {
         100: 'rgb(var(--purple-100) / <alpha-value>)',
       }
     },
-    spacing: {
-      // This spacing scale is based on the actual pixel values converted to REM values.
-      // This diverges from tailwind's standard pattern of defining its own spacing numbers
-      // but should be easier for developers trying to match spacing to designs in Figma.
-      0: '0',
-      1: '0.0625rem', // 1px
-      3: '0.1875rem', // 3px
-      5: '0.3125rem', // 5px
-      10: '0.625rem', // 10px
-      15: '0.9375rem', // 15px
-      20: '1.25rem', // 20px
-      25: '1.5625rem', // 25px
-      30: '1.875rem', // 30px
-      35: '2.1875rem', // 35px
-      40: '2.5rem', // 40px
-      45: '2.8125rem', // 45px
-      50: '3.125rem', // 50px
-      60: '3.75rem', // 60px
-    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
   darkMode: 'class',
 };
