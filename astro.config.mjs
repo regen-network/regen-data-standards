cef27afecfd5f1e5b517d2aa47b39e4e0ffa0998
Aaron Craelius <aaronc@users.noreply.github.com>
2024-03-04 14:59:23 -0500

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({})],
+    integrations: [
+        tailwind({}),
+        pagefind()
+    ],
 });
