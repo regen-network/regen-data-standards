bdd70fe2d93b08f2a21ec1d39d6f9ec3ed7b1346
S4mmyb <sambennetts54@gmail.com>
2024-10-20 18:43:09 -0600

    sidebar with dropdowns for subcategories

---
+++ src/data/schemaCategories.ts
@@ -0,0 +1,21 @@
+export const schemaCategories = [
+    {
+        category: "Projects",
+        subCategories: [
+            {
+                category: "Generic Schemas",
+                entries: [
+                    { slug: 'ProjectPost', title: 'ProjectPost' },
+                    { slug: 'ProjectInfo', title: 'ProjectInfo' }
+                ]
+            },
+            {
+                category: "Partner-Specific Schemas",
+                entries: [
+                    { slug: 'TerrasosProjectInfo', title: 'TerrasosProjectInfo' }
+                ]
+            }
+        ],
+
+    }
+];
